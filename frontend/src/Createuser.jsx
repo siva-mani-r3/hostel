876e05751cc8749340512a697e423f37e3b5c05c
siva-mani-r3 <99952847+siva-mani-r3@users.noreply.github.com>
2024-11-05 18:46:27 +0530

    Update Createuser.jsx

--- frontend/src/Createuser.jsx
+++ frontend/src/Createuser.jsx
@@ -7,7 +7,7 @@ const Createuser = () => {
     const navigate = useNavigate()
     const Submit=(e)=>{
         e.preventDefault();
-        axios.post('http://localhost:3002/create',{name,rollno})
+        axios.post('https://hostel-backend-e7gv.onrender.com/create',{name,rollno})
         .then(result=>{console.log(result)
             navigate('/users1')
         })
