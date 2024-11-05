b84ca9f660b7b40fbeb721bcad35e6462a41396c
siva-mani-r3 <99952847+siva-mani-r3@users.noreply.github.com>
2024-11-05 19:07:41 +0530

    Update Users5.jsx

--- frontend/src/Users5.jsx
+++ frontend/src/Users5.jsx
@@ -4,14 +4,14 @@ import axios from 'axios'
 function Users5 ()  {
 const[users5,setusers5]=useState([]) 
 useEffect(()=>{
-    axios.get('http://localhost:3002/users5')
+    axios.get('https://hostel-backend-e7gv.onrender.com/users5')
     .then(result=>setusers5(result.data))
     .catch(err=>console.log(err))
 
 
 },[])
 const handleDelete =(id)=>{
-    axios.delete('http://localhost:3002/deleteUser5/'+id)
+    axios.delete('https://hostel-backend-e7gv.onrender.com/deleteUser5/'+id)
     .then(res=>{console.log(res)
 window.location.reload()})
     .catch(err=>console.log(err))
